5b9ac02723a2bc82f207cc0adf9a5f98b4f8978f
Amar Asagekar <amarasagekar@gmail.com>
2024-01-02 21:28:07 +0530

    done with pages, added mediaquery to navbar

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,8 @@ import { ShopContext } from "../../Context/ShopContext";
 import remove_icon from "../Assets/cart_cross_icon.png";
 
 const CartItems = () => {
-  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  const { getTotalCartAmout, all_product, cartItems, removeFromCart } =
+    useContext(ShopContext);
   return (
     <div className="cartitems">
       <div className="cartitems-format-main">
@@ -45,15 +46,31 @@ const CartItems = () => {
       })}
       <div className="cartitems-down">
         <div className="cartitems-total">
-            <h1>CartTotals</h1>
-            <div>
-                <div className="cartitems-total-item">
-                    <p>Subtotal</p>
-                    <p>${0}</p>
-                </div>
-                <hr/>
-                <div className="cartitems-total-item"></div>
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmout()}</p>
             </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shipping fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <h3>Total</h3>
+              <h3>${getTotalCartAmout()}</h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promocode enter it here</p>
+          <div className="cartitems-promobox">
+            <input type="text" placeholder="promo code" />
+            <button>Submit</button>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -5,7 +5,7 @@ export const ShopContext = createContext(null);
 
 const getDeafultCart = () => {
   let cart = {};
-  for (let index = 0; index < all_product.length+1; index++) {
+  for (let index = 0; index < all_product.length + 1; index++) {
     cart[index] = 0;
   }
   return cart;
@@ -15,13 +15,47 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDeafultCart());
 
   const addToCart = (itemId) => {
-    setCartItems((prev) => ({...prev,[itemId]: prev[itemId]+1 }));
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     console.log(cartItems);
   };
   const removeFromCart = (itemId) => {
-    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId]-1 }));
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
-  const contextValue = { all_product, cartItems, addToCart, removeFromCart };
+
+  const getTotalCartAmout = () => {
+    let totalAmount = 0;
+    console.log("cartItems===" + cartItems.length);
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        console.log("itemInfo.new_price===" + itemInfo.new_price);
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
+      console.log("totalAmount===" + totalAmount);
+    }
+    return totalAmount;
+  };
+
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
+
+  const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmout,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
